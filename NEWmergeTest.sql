fd15c4456c947c3d92da130f0aad38ad28d62338
fhernan1 <analyst.francisco@gmail.com>
2020-02-05 16:01:52 -0500

    Update NEWmergeTest.sql

--- NEWmergeTest.sql
+++ NEWmergeTest.sql
@@ -1,28 +1,36 @@
---whoami=userID as schema for temp table
+--USERID=userID as schema for temp table
 --HEADER=first line in CSV
---PASTED= user inputs schema and target table
+--SCHEMATAB= user inputs schema and target table
+CREATE TABLE ${USERID}.FW_TEMP AS (
+SELECT
+	${HEADER}
+FROM
+	${SCHEMATAB} WITH NO DATA;
 
-CREATE TABLE `whoami`.FW_TEMP
-	AS
-		(SELECT 'HEADER'
-			FROM 'PASTED') WITH NO DATA;
-exit code
-INSERT INTO `whoami`.FW_TEMP
-	('HEADER')
-	VALUES
-	(''''''''''');
-exit code (Handle duplicates in file)
+--EXIT code
+INSERT
+	INTO
+	`whoami`.FW_TEMP ('HEADER')
+VALUES ('','','','','');
+--exit code (Handle duplicates in file)
 
-MERGE INTO 'PASTED' c
-   USING `whoami`.FW_TEMP e
-   ON c.key1=e.key1 AND c.key2=e.key2 AND c.key3=e.key3
-WHEN NOT MATCHED THEN 
-     INSERT
-        (c.HEADER1,...)
-     VALUES
-        (e.HEADER1,...)
-WHEN MATCHED THEN UPDATE
-       SET c.HEADER1 = e.HEADER1,
-           c.HEADER2 = e.HEADER2,
-		--and so on
- ;
+MERGE INTO ${SCHEMATAB} tgt
+	USING ${USERID}.FW_TEMP src ON
+src.key1 = tgt.key1
+AND src.key2 = tgt.key2
+AND src.key3 = tgt.key3
+WHEN NOT MATCHED THEN
+INSERT
+	(HEADER1,
+	...)
+VALUES (src.HEADER1,
+...)
+WHEN MATCHED THEN
+UPDATE
+SET
+	tgt.HEADER1 = src.HEADER1,
+	tgt.HEADER2 = src.HEADER2,
+	--and so on
+;
+--exit code into if statement
+DROP TABLE ${USERID}.FW_TEMP
